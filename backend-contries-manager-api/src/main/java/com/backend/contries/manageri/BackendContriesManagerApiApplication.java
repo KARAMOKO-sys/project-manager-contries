1140664df65153b8c1eea5cfb2a138058933f24f
LendroDuCode <deto.karamoko@sonecafrica.com>
2025-11-16 18:09:14 +0000

    Premier commit

---
+++ backend-contries-manager-api/src/main/java/com/backend/contries/manageri/BackendContriesManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.backend.contries.manageri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendContriesManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendContriesManagerApiApplication.class, args);
+	}
+
+}
---
+++ backend-contries-manager-api/src/test/java/com/backend/contries/manageri/BackendContriesManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.contries.manageri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendContriesManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
